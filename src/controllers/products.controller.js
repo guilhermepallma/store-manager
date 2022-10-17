52736dbeb8acc197a46a46e9cbef8443174a7c73
Guilherme Palma <guilhermepallma@gmail.com>
2022-10-17 17:44:34 -0400

    🛠 resolvendo bug de multiplos retorno

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -2,7 +2,7 @@ const { productsService } = require('../services');
 
 const getAllController = async (_request, response) => {
   const result = await productsService.getAllService();
-  response.status(200).json(result);
+  return response.status(200).json(result);
 };
 
 const getByIdController = async (request, response) => {
@@ -10,9 +10,9 @@ const getByIdController = async (request, response) => {
   const result = await productsService.getByIdService(id);
 
   if (result) {
-    response.status(200).json(result);
+    return response.status(200).json(result);
   }
-    response.status(404).json({ message: 'Product not found' });
+    return response.status(404).json({ message: 'Product not found' });
 };
 
 const createNewProduct = async (request, response) => {
